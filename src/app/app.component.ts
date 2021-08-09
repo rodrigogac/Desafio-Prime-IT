605017d2ceed1d58d70b3576f17737bfd2ed4341
unknown <Administrador@STFSAON006711-L.stefanini.dom>
2021-08-08 21:42:44 -0300

    desafio IT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatCardModule} from '@angular/material/card';  
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'desafio-pokemon-rodrigo';
+  title = 'Desafio Pokemon Rodrigo';
+  
+  names = [];
+  cards = [];
+  ids = [];
+  tipos = [];
+  
+  url = 'https://api.pokemontcg.io/v2/cards';
+
+  
+
+
+  constructor(private http: HttpClient) {
+	this.http.get<any>(this.url).toPromise().then(dados => {
+		
+		console.log(dados.data);
+		
+		for (let id in dados.data)
+			this.cards.push(dados.data[id]['images'].large as never);
+		
+		for (let id in dados.data)
+			this.names.push(dados.data[id]['name'] as never);
+		
+		for (let id in dados.data)
+			this.ids.push(dados.data[id]['id'] as never);
+		
+		for (let id in dados.data)
+			this.tipos.push(dados.data[id]['types'] as never);
+	
+		
+	}); 
+  }
+
+
+
+  
+  
 }
